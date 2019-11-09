4f673378114745cb9a8c9aadc8b16085256e28ed
inql <dbinkus96@gmail.com>
2019-11-09 23:43:26 +0100

    General refactor of test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,22 +1,25 @@
 package com.gildedrose;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class GildedRoseTest {
-    
+
+    private GildedRose app;
+
     @Test
     public void itemHasSpecifiedType() {
-        GildedRose app = createAppWithSingleItem("foo", 0, 0);
-        assertEquals("foo", app.items[0].name);
+        app = createAppWithSingleItem("foo", 0, 0);
+        assertThat(getLoneItem().name, is("foo"));
     }
 
     @Test
     public void typeRemainsUnchangedAtEndOfDay() {
-        GildedRose app = createAppWithSingleItem("foo", 0, 0);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        app = createAppWithSingleItem("foo", 0, 0);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().name, is("foo"));
     }
 
     private GildedRose createAppWithSingleItem(String name, int sellIn, int quality) {
@@ -25,6 +28,15 @@ public class GildedRoseTest {
 
     private Item[] createSingleItemArray(String name, int sellIn, int quality) {
         return new Item[] { new Item(name, sellIn, quality) };
+    }
+
+    private Item getLoneItem() {
+        assert app.items.length == 1 : "Expecting exactly one item";
+        return getFirstItem();
+    }
+
+    private Item getFirstItem() {
+        return app.items[0];
     }
 
 }
