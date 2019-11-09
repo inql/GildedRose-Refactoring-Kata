1bc5f4948f3f553276eb06ba2e436386276d6e57
inql <dbinkus96@gmail.com>
2019-11-09 23:40:45 +0100

    Split test cases in GildedRoseTest.java

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,20 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
+    public void itemHasSpecifiedType() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
+        assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    public void typeRemainsUnchangedAtEndOfDay(){
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        assertEquals("foo", app.items[0].name);
     }
 
 }
